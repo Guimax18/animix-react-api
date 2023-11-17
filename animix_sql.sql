db250db88e7d5f1b36dee954e73e2b99bf46cb7a
Guilherme Max <guimaxgui16@email.com>
2023-11-16 23:42:50 -0300

    Primeiro commit

---
+++ animix_sql.sql
@@ -0,0 +1,14 @@
+create database animix;
+use animix;
+
+CREATE TABLE tb_roupas (
+	id_roupa   int primary key auto_increment,
+    nm_roupa   varchar(200),
+    categoria  varchar(200),
+    tamanho	   varchar(50),
+	valor	   decimal(1000),
+	cor		   varchar(50),
+    anime	   varchar(200)
+);
+
+SELECT * FROM tb_roupas
